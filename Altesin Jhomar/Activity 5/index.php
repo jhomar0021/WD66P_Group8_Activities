e807ee5e216b86a5449f3852e8f3348bbd47f1fc
Jhomar Altesin <altesin.jhomar@gmail.com>
2023-06-10 20:34:34 +0800

    changed variable name for readability

--- Altesin Jhomar/Activity 5/index.php
+++ Altesin Jhomar/Activity 5/index.php
@@ -11,25 +11,25 @@ echo getTotalStock(2,$item1);
 echo getTotalStock(2,$item2);
 echo getTotalStock(5,$item3);
 
-function getTotalStock($qty,$stocks){
+function getTotalStock($qty,$item){
   global $item1;
   global $item2;
   global $item3;
   
-    if ($stocks == $item1){
-      $item1 = $stocks - $qty;
+    if ($item == $item1){
+      $item1 = $item - $qty;
   
       echo "item 1 - ($qty) qty | Remaining stocks = $item1";
       echo "\n";
     }
-    else if ($stocks == $item2){
-      $item2 = $stocks - $qty;
+    else if ($item == $item2){
+      $item2 = $item - $qty;
   
       echo "item 2 - ($qty) qty | Remaining stocks = $item2";
       echo "\n";
     }
-    else if ($stocks == $item3){
-      $item3 = $stocks - $qty;
+    else if ($item == $item3){
+      $item3 = $item - $qty;
   
       echo "item 3 - ($qty) qty | Remaining stocks = $item3";
       echo "\n";
