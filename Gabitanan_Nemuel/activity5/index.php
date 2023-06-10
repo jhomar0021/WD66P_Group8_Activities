dd73a847a91cac7f9b487912a2b2d6dbbf8061fb
Nemuel M Gabitanan Jr <nemuelmgabitananjr@gmail.com>
2023-06-10 11:12:15 +0800

    Activity 5 of Nemuel Gabitanan Jr - for checking

---
+++ Gabitanan_Nemuel/activity5/index.php
@@ -0,0 +1,29 @@
+<?php
+  $item1 = 100;
+  $item2 = 200;
+  $item3 = 300;
+  
+	echo getGroceryItem("Item 1", 3, $item1);
+	echo getGroceryItem("Item 1", 5, $item1); 
+	echo getGroceryItem("Item 2", 5, $item2); 
+	echo getGroceryItem("Item 1", 50, $item1); 
+	echo getGroceryItem("Item 3", 5, $item3);
+
+	
+	function getGroceryItem($item, $qty, $stockqty){
+	  global $item1, $item2, $item3;
+	  if ($stockqty == $item1){
+	    $stockqty = $item1 - $qty;
+	    return "$item - ($qty) qty | Remaining Stocks = $stockqty \n";
+	  }
+	  if ($stockqty == $item2){
+	    $stockqty = $item2 - $qty;
+	    return "$item - ($qty) qty | Remaining Stocks = $stockqty \n";
+	  }
+	  if ($stockqty == $item3){
+	    $stockqty = $item3 - $qty;
+	    return "$item - ($qty) qty | Remaining Stocks = $stockqty \n";
+	  }
+	  
+	}
+?>
